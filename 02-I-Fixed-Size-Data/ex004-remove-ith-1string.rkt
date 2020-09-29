ea0f8e0e5b553af227b11848fcca56293c731365
Fernando Basso <fernandobasso.br@gmail.com>
2020-09-29 20:57:47 -0300

    Rename exercise files

--- 02-I-Fixed-Size-Data/ex004-remove-ith-1string.rkt
+++ 02-I-Fixed-Size-Data/ex004-remove-ith-1string.rkt
@@ -1,8 +1,8 @@
 #lang htdp/bsl
 
 ;;
-;; Exercise 3:
-;; https://htdp.org/2020-8-1/Book/part_one.html#%28counter._%28exercise._arith-s1%29%29
+;; Exercise 4:
+;; https://htdp.org/2020-8-1/Book/part_one.html#%28counter._%28exercise._arith-s2%29%29
 ;;
 
 (define str "hello-world")
