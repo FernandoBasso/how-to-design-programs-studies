cf0f07a54dcd733848977fe093c3a14b476247e6
Fernando Basso <fernandobasso.br@gmail.com>
2020-09-29 21:52:04 -0300

    Exercise 007 on bools

---
+++ 02-I-Fixed-Size-Data/ex007-or-bools.rkt
@@ -0,0 +1,13 @@
+#lang htdp/bsl
+
+;;
+;; Exercise 7:
+;; https://htdp.org/2020-8-1/Book/part_one.html#%28counter._%28exercise._arith-b1%29%29
+;;
+
+(define sunny #t)
+
+(define friday #f)
+
+(or (not sunny) friday)
+
