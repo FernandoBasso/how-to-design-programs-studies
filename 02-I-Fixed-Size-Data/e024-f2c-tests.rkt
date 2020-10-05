020387da0195cdf35ee7c6fbc95c1387e387320b
Fernando Basso <fernandobasso.br@gmail.com>
2020-10-05 19:06:12 -0300

    Finish section on testing

---
+++ 02-I-Fixed-Size-Data/e024-f2c-tests.rkt
@@ -0,0 +1,14 @@
+#lang htdp/bsl
+
+; Number -> Number
+; converts Fahrenheit temperatures to Celsius
+; given 32, expect 0
+; given 212, expect 100
+; given -40, expect -40
+(define (f2c f)
+  (* 5/9 (- f 32)))
+
+(check-expect (f2c -40) -40)
+(check-expect (f2c 32) 0)
+(check-expect (f2c 212) 100)
+
