ec097e4eddb7f4842313d5ca677e61ff6a7bc123
Fernando Basso <fernandobasso.br@gmail.com>
2020-10-11 11:55:14 -0300

    A world program starter template

---
+++ world-program-template.rkt
@@ -0,0 +1,52 @@
+#lang htdp/bsl
+;(require 2htdp/image)
+;(require 2htdp/universe)
+
+;
+; EXERCISE <number>:
+; <url here>
+;
+; ...any notes or further information‽...
+;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PHYSICAL AND GRAPHICAL CONSTANTS
+;;
+
+;;
+; All other scaling and dimensions are based on this value.
+;
+(define CW 400)
+
+;;
+; The height is “one fourth” of the width.
+;
+(define CH (* 1/4 CW))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FUNCTIONS
+;;
+
+;;
+; The canvas width and height.
+;
+
+;;
+; tick
+;
+
+;;
+; stop?
+;
+
+;;
+; render
+
+;;
+; main
+;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ...ANY ADDITIONAL NOTES‽...
+;;
+
