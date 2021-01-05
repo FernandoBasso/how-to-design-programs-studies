07e671faf181b44287933bee16c2fda038e8027b
Fernando Basso <fernandobasso.br@gmail.com>
2021-01-04 21:51:04 -0300

    Very simple exercise 52.

---
+++ 02-I-Fixed-Size-Data/e075p52-open-closed-intervals.rkt
@@ -0,0 +1,12 @@
+;
+; EXERCISE 52
+;
+; [3, 5] is a closed intervals. Includes, 3, 4 and 5.
+;
+; (3, 5] is a left-open interval. Includes 4 and 5.
+;
+; [3, 5) is a right-open interval. Includes 3 and 4.
+;
+; (3, 5) is an open interval. Includes only 4.
+;
+
